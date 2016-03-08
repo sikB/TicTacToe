34e7eb6f062baf5913fe83dea89c73ea7a65e7a2
sikB <sikhovski@icloud.com>
2016-03-08 14:00:19 -0500

    Update
    displays messages if player one wins. detects a winner through a for
    loop from an array

--- tictactoe.js
+++ tictactoe.js
@@ -33,38 +33,72 @@ var winners = [
 ['a1' ,'b1', 'c1'],
 ['a2' ,'b2', 'c2'],
 ['a3' ,'b3', 'c3'],
-
 ['a1' ,'b2', 'c3'],
 ['a3' ,'b2', 'c1'],
 
 ];
 
+var playerOneMarkings = [];
+var playerTwoMarkings = [];
 var whosTurn = 1;
 
 function addSymbol(element){
 	var gameHeader = document.getElementById('game-header')
 	if (element.innerHTML == '') {
-		gameHeader.className = '';
 		if(whosTurn == 1){
 			element.innerHTML = 'X';
 			whosTurn = 2;
 			gameHeader.innerHTML = "Its Player 2's turn";
-			gameHeader.className = 'player2';}
+			gameHeader.className = 'player-two';
+			element.classList.remove('empty');
+			element.classList.add('p1');
+			playerOneMarkings.push(element.id);
+		}
 			else{
 				element.innerHTML = 'O';
 				whosTurn = 1;
 				gameHeader.innerHTML = "Its Player 1's turn";
+				gameHeader.className = 'player-one';
+				element.classList.remove('empty');
+				element.classList.add('p2');
+				playerTwoMarkings.push(element.id);
 			}
 		}
 	else{
-		var gameHeader = document.getElementById('game-header')
 		gameHeader.innerHTML = "This box is taken";
 		gameHeader.className = "red";
 	}
+	checkWin();
 }
 
+function checkWin(){
+	var rowCount = 0;
+	var thisWinCombination;
+	for(i=0; i < winners.length; i++){
+		rowCount = 0;
+		thisWinCombination = winners[i];
+		for(j=0; j<thisWinCombination.length; j++){
+			if(playerOneMarkings.indexOf(thisWinCombination[j]) > -1){
+				rowCount++;
+			}
+		}
+
+		if(rowCount === 3){
+			gameOver(thisWinCombination);
+		}
+	}
 
 
+}
+
+function gameOver(combo){
+	var gameHeader = document.getElementById('game-header');
+	for(i=0; i<combo.length; i++){
+		document.getElementById(combo[i]).classList.add('winner');
+	}
+	gameHeader.innerHTML = 'Player One, won the game!';
+	
+}
 var squareWidth = document.getElementById('a1').clientWidth;
 var squares = document.getElementsByClassName('squares');
 for(i=0; i < a1.length; i++){
