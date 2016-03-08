9214cfe1b76afbbc980658dbd74e4c4584176b78
sikB <sikhovski@icloud.com>
2016-03-07 21:13:44 -0500

    TicTacToe
    made the grid. made the grid pieces clickable. made the x’s show up

---
+++ tictactoe.js
@@ -0,0 +1,38 @@
+function aOne(){
+	document.getElementById('xa1').style.visibility = "visible";
+}
+function aTwo(){
+	document.getElementById('xa2').style.visibility = "visible";
+}
+function aThree(){
+	document.getElementById('xa3').style.visibility = "visible";
+}
+function bOne(){
+	document.getElementById('xb1').style.visibility = "visible";
+}
+function bTwo(){
+	document.getElementById('xb2').style.visibility = "visible";
+}
+function bThree(){
+	document.getElementById('xb3').style.visibility = "visible";
+}
+function cOne(){
+	document.getElementById('xc1').style.visibility = "visible";
+}
+function cTwo(){
+	document.getElementById('xc2').style.visibility = "visible";
+}
+function cThree(){
+	document.getElementById('xc3').style.visibility = "visible";
+}
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
